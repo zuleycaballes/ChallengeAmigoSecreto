39143feab909b11eb755837b1f5c0ee2172c1b89
zuleycaballes <A01741687@tec.mx>
2025-08-11 17:29:27 -0700

    lista amigos y funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+
+    if (nombre == '') {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nombre);
+    }
+
+    document.getElementById.apply('amigo').value = '';
+}
+
+function actualizarLista() {
+    
+}

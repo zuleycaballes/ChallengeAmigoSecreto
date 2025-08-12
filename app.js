0f8e71e04d4bdfa194878dcf8f2abc71b47df4ec
zuleycaballes <A01741687@tec.mx>
2025-08-11 19:56:00 -0700

    implementar funcion actualizar lista

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let amigos = [];
+let amigos = []; 
+let totalAmigos = amigos.length;
 
 function agregarAmigo() {
     let nombre = document.getElementById('amigo').value;
@@ -7,11 +8,20 @@ function agregarAmigo() {
         alert("Por favor, inserte un nombre.");
     } else {
         amigos.push(nombre);
+        actualizarLista();
     }
 
-    document.getElementById.apply('amigo').value = '';
+    document.getElementById('amigo').value = '';
+    totalAmigos = amigos.length;
 }
 
 function actualizarLista() {
-    
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+  
+    for (i = 0; i <= totalAmigos; i++) {
+        let elemento = document.createElement('li');
+        elemento.innerHTML = amigos[i];
+        lista.appendChild(elemento);
+    }
 }
